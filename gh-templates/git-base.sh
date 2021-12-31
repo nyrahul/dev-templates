761bd7fd48834a65a8d220bcd948a3e663f2dcd0
Rahul Jadhav <nyrahul@gmail.com>
2021-12-31 09:31:30 +0530

    SUBMODULE: commit_title
    Description
    
    Fixes: #3
    
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

--- gh-templates/git-base.sh
+++ gh-templates/git-base.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-NC='\033[0m' # No Color
+ORANGE="\033[0;33m"
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+CYAN="\033[0;36m"
+NC="\033[0m" # No Color
 
 repo=$(basename $(git remote get-url origin) .git)
 
+statusline()
+{
+	status=$1
+	shift
+	[[ $status == AOK ]] || [[ $status == "0" ]] &&
+		{
+			printf "[${GREEN}OK${NC}] $*\n"
+			return
+		}
+	[[ $status == WARN ]] &&
+		{
+			printf "[${ORANGE}WARN${NC}] $*\n"
+			return
+		}
+	[[ $status == WAIT ]] &&
+		{
+			printf "[${CYAN}..${NC}] $*\r"
+			return
+		}
+	printf "[${RED}FAIL${NC}] $*\n"
+	exit 1
+}
+
+checkGithubAuthStatus()
+{
+	[[ "$(which gh)" == "" ]] && statusline ERR "github cli-tool 'gh' not found in PATH. Download and Install from https://github.com/cli/cli/releases"
+	str=`gh auth status 2>&1`
+	[[ $? -ne 0 ]] && echo "$str" && statusline ERR "'gh auth status' does not show you are logged in. Use 'gh auth login'."
+}
+
+checkGithubAuthStatus
